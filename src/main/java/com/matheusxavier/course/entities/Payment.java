87efee6b92a5a62136cf7802f3c0128425bfe354
matheusxds123 <101800697+matheusxds123@users.noreply.github.com>
2023-05-19 14:34:43 -0300

    Subtotal e Total methods

--- src/main/java/com/matheusxavier/course/entities/Payment.java
+++ src/main/java/com/matheusxavier/course/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
@@ -52,7 +55,7 @@ public class Payment implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+	
 	public Order getOrder() {
 		return order;
 	}
