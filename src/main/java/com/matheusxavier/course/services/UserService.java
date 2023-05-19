2dfa9525be481f77f7ff93709b1daa8cd34929d2
matheusxds123 <101800697+matheusxds123@users.noreply.github.com>
2023-05-19 14:49:18 -0300

    User insert

--- src/main/java/com/matheusxavier/course/services/UserService.java
+++ src/main/java/com/matheusxavier/course/services/UserService.java
@@ -17,11 +17,15 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
-	};
+	}
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
 	}
 }
 
